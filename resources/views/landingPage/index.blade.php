2c6bf41785fc51693310af853bc78d5ba64d3936
davidtoreh <davidtoreh12@gmail.com>
2024-03-21 21:53:13 +0700

    RPL-5 Profil Perusahaan (tambahan)

---
+++ resources/views/landingPage/index.blade.php
@@ -0,0 +1,103 @@
+@extends('landingPage.layouts.app')
+@section('title', 'Sate Balibul')
+@section('content')
+
+    <!--====== ABOUT PART START ======-->
+
+    <section id="about" class="about_area pt-120 pb-130">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-6">
+                    <div class="section_title text-center pb-10">
+                        <h4 class="title">About Us</h4>
+                        <span class="line">
+                            <span class="box"></span>
+                        </span>
+                    </div> <!-- section title -->
+                </div>
+            </div> <!-- row -->
+            <div class="row align-items-center">
+                <div class="col-lg-6">
+                    <div class="about_image mt-50 wow fadeInRightBig" data-wow-duration="1.3s" data-wow-delay="0.5s">
+                        <img src="images/portrait.jpg" alt="about">
+                    </div> <!-- about image -->
+                </div>
+                <div class="col-lg-6">
+                    <div class="about_content mt-45 wow fadeInLeftBig" data-wow-duration="1.3s" data-wow-delay="0.5s">
+                        <h4 class="about_title">Tentang Sate Balibul</h4>
+                        <p>Rumah Makan Sate Balibul didirikan dengan cinta dan dedikasi untuk memperkenalkan kelezatan sate
+                            kambing Indonesia ke seluruh penjuru. Pendiri kami, Ibu Tabita dan Bapak Ali, adalah seorang
+                            pengusaha yang telah menjalani perjalanan kuliner mereka dengan kehidupan dan warisan yang kuat.
+                            Mereka membawa pengetahuan mereka dalam meracik dan memanggang sate kambing yang sempurna,
+                            dengan resep warisan keluarga yang telah turun-temurun.</p>
+                        <ul class="social">
+                            <li><a href="https://wa.me/082224000250"><i class="lni lni-whatsapp"></i></a></li>
+                            <li><a href="https://www.instagram.com/satebalibulbangali/"><i class="lni lni-instagram-original"></i></a></li>
+                        </ul>
+                    </div> <!-- row -->
+                </div>
+            </div> <!-- row -->
+        </div> <!-- container -->
+    </section>
+
+    <!--====== ABOUT PART ENDS ======-->
+
+
+    <!--====== COUNTER PART START ======-->
+
+    <section id="counter" class="counter_area pt-50 pb-95 text-center" style="background-image: url(images/sate\ wallpaper.jpg)">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-sm-12">
+                    <div class="single_counter mt-30 wow fadeIn" data-wow-duration="1.3s" data-wow-delay="0.2s">
+                        <span class="count"><span class="counter">36546</span></span>
+                        <p>Satay Served</p>
+                    </div> <!-- single counter -->
+                </div>
+                <div class="col-md-4 col-sm-12">
+                    <div class="single_counter mt-30 wow fadeIn" data-wow-duration="1.3s" data-wow-delay="0.5s">
+                        <span class="count"><span class="counter">6</span></span>
+                        <p>Type of Satay</p>
+                    </div> <!-- single counter -->
+                </div>
+                <div class="col-md-4 col-sm-12">
+                    <div class="single_counter mt-30 wow fadeIn" data-wow-duration="1.3s" data-wow-delay="0.8s">
+                        <span class="count"><span class="counter">12</span></span>
+                        <p>Team Members</p>
+                    </div> <!-- single counter -->
+                </div>
+            </div> <!-- row -->
+        </div> <!-- container -->
+    </section>
+
+    <!--====== COUNTER PART ENDS ======-->
+
+
+    <!--====== MENU PART START ======-->
+
+    
+
+    <!--====== MENU PART ENDS ======-->
+
+
+    <!--====== CUSTOMER PART START ======-->
+
+    
+
+    <!--====== CUSTOMER PART ENDS ======-->
+
+
+    <!--====== CONTACT PART START ======-->
+
+    
+
+    <!--====== CONTACT PART ENDS ======-->
+
+
+    <!--====== OUTLET PART START======-->
+
+    
+
+    <!--====== OUTLET PART ENDS ======-->
+
+@endsection
---
+++ resources/views/landingPage/layouts/app.blade.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <title>@yield('title')</title>
+    
+    <link href="https://fonts.googleapis.com/css?family=Philosopher:400,700|Poppins:300,400,500,600,700,800&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="{{asset('frontend')}}/css/animate.css">
+    
+    <link rel="stylesheet" href="{{asset('frontend')}}/css/bootstrap.css">
+    <link rel="stylesheet" href="{{asset('frontend')}}/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="{{asset('frontend')}}/css/default.css">
+
+    <link rel="stylesheet" href="{{asset('frontend')}}/css/LineIcons-min.css">
+    <link rel="stylesheet" href="{{asset('frontend')}}/css/LineIcons.css">
+
+    <link rel="stylesheet" href="{{asset('frontend')}}/css/slick.css">
+
+    <link rel="stylesheet" href="{{asset('frontend')}}/css/style-min.css">
+    <link rel="stylesheet" href="{{asset('frontend')}}/css/style.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+
+  </head>
+  <body>
+  	
+  @include('landingPage.layouts.partial.header')
+
+  @yield('content')                  
+
+  @include('landingPage.layouts.partial.footer')
+ 
+  <script src="{{asset('frontend')}}/js/vendor/jquery-3.5.1.min.js"></script>
+  <script src="{{asset('frontend')}}/js/vendor/modernizr-3.7.1.min.js"></script>
+
+  <script src="{{asset('frontend')}}/js/boostrap.js"></script>
+  <script src="{{asset('frontend')}}/js/jquery.easing.min.js"></script>
+  <script src="{{asset('frontend')}}/js/main.js"></script>
+  <script src="{{asset('frontend')}}/js/popper.min.js"></script>
+  <script src="{{asset('frontend')}}/js/scrolling-nav.js"></script>
+  <script src="{{asset('frontend')}}/js/slick.min.js"></script>
+  <script src="{{asset('frontend')}}/js/wow.min.js"></script>
+
+ <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+        
+</html>
+
+ 
