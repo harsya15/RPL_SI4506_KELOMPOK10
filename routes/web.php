e0fb3c4ad6c5cee0acb0a004733d7d7d0e19fb35
harsya15 <arvinharsya@gmail.com>
2024-05-13 11:44:14 +0700

    RPL-20 pbi#18

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('landingPage.index');
 });
+
+Route::get('/Cabang', [CabangController::class, 'index'])->name('cabang.index');
+Route::get('/Cabang/create', [CabangController::class, 'create'])->name('cabang.create');
+Route::get('/Cabang/edit/{id}', [CabangController::class, 'edit'])->name('cabang.edit');
+Route::post('/Cabang/store', [CabangController::class, 'store'])->name('cabang.store');
+Route::post('/Cabang/update/{id}', [CabangController::class, 'update'])->name('cabang.update');
+Route::delete('/Cabang/delete/{id}', [CabangController::class, 'delete'])->name('cabang.delete');
