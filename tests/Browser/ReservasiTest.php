2e9fe961083a0760cc2ad0f601b211cf9a79f35a
Indrarachman12 <indrarachman28@gmail.com>
2024-05-21 14:45:15 +0700

    push change reservasi

---
+++ tests/Browser/ReservasiTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class ReservasiTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     */
+    public function testExample(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser-visit('/')
+            ->assertSee('Reservation')
+            ->type('no_meja', '10')
+            ->type('atas_nama', 'John Doe')
+            ->type('date', '2024-05-21')
+            ->type('time', '18:30')
+            ->type('jumlah_orang', '4')
+            ->type('email', 'johndoe@example.com')
+            ->press('Submit');
+        });
+    }
+}
