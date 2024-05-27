f2b1da06af1574b05068583b90bc49a2661a2e14
davidtoreh <davidtoreh12@gmail.com>
2024-05-27 19:28:47 +0700

    RPL-17 Mulai mengerjakan fitur mengelola reservasi

--- app/Http/Middleware/ManagerMiddleware.php
+++ app/Http/Middleware/ManagerMiddleware.php
@@ -13,6 +13,7 @@ class ManagerMiddleware
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
+    
     public function handle(Request $request, Closure $next): Response
     {
         if (auth()->user()->role == 'manager')
