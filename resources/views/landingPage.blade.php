42e493a8de79db26d23882f66bf29c737ab6368f
mufhidaturrahmahhhh <mufhidaturrahmah@student.telkomuniversity.ac.id>
2024-06-03 21:57:37 +0700

    RPL-32 menyelesaikan PBI#36

--- resources/views/landingPage.blade.php
+++ resources/views/landingPage.blade.php
@@ -324,6 +324,7 @@
     <!--====== COUNTER PART ENDS ======-->
 
     <!--====== GALERI FOTO PART START======-->
+    
 
     <section id="gallery" class="gallery pt-120 pb-10">
         <div class="section_title text-center pb-50">
